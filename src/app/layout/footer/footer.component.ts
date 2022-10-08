8b190fa671bdaa69479b5839b8611fc7d3c112d4
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2022-10-08 14:17:48 +0200

    Replace links in footer with link component

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { LinkComponent } from 'src/shared/components';
 
 @Component({
   standalone: true,
+  imports: [LinkComponent],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
