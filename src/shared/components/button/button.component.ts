5612246dc324e9592e5fc687f99ddb8b4168eeed
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2022-10-08 18:51:27 +0200

    Now can add icon to button

--- src/shared/components/button/button.component.ts
+++ src/shared/components/button/button.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ComponentColor } from '../../types';
 import { ButtonTypes } from '../../types';
+import { ImgComponent } from '../img';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImgComponent],
   selector: 'styled-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
@@ -13,6 +14,9 @@ import { ButtonTypes } from '../../types';
 export class ButtonComponent {
   @Input() public color: ComponentColor = ComponentColor.Primary;
   @Input() public type: ButtonTypes = ButtonTypes.Button;
-  @Input() public isIcon = false;
+  @Input() public icon: string;
+  @Input() public iconHeight = 20;
+  @Input() public iconWidth = 20;
+  @Input() public noPadding = false;
   @Input() public isDisabled = false;
 }
